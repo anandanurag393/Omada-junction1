efb41c314654ced0585aae10ee1e65b82c9bb1cc
A-R-Khan <abdulrahimkhan1999@gmail.com>
2020-02-05 22:56:47 +0530

    changed more screen layout and cardscroller

--- app/src/main/java/com/omada/junction/JunctionCard.java
+++ app/src/main/java/com/omada/junction/JunctionCard.java
@@ -1,13 +1,14 @@
 package com.omada.junction;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 
 public class JunctionCard extends LinearLayout {
 
-    public JunctionCard(Context context){
-        super(context);
+    public JunctionCard(Context context, AttributeSet attrs){
+        super(context, attrs);
         LayoutInflater inflater = LayoutInflater.from(context);
         inflater.inflate(R.layout.card_layout, this, false);
     }
