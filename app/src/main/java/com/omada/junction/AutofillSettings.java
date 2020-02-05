50de3100f3dc34259e82677b77d1945ae8268e31
aditya <Walnut>
2020-02-05 23:32:42 +0530

    made autofillSettings layout and java file

---
+++ app/src/main/java/com/omada/junction/AutofillSettings.java
@@ -0,0 +1,15 @@
+package com.omada.junction;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+public class AutofillSettings extends LinearLayout
+{
+
+    public AutofillSettings(Context context) {
+        super(context);
+        LayoutInflater layoutInflater= LayoutInflater.from(context);
+        layoutInflater.inflate(R.layout.autofill_setttings,this,true);
+    }
+}
