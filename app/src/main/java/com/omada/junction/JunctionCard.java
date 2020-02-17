f68bfbd55364c1661dc9f1646b7b775f146d849a
Vivann <7vivann@gmail.com>
2020-02-17 19:17:18 +0530

    3

--- app/src/main/java/com/omada/junction/JunctionCard.java
+++ app/src/main/java/com/omada/junction/JunctionCard.java
@@ -2,14 +2,18 @@ package com.omada.junction;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
-import android.widget.LinearLayout;
+import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
+import android.widget.RelativeLayout;
 
-public class JunctionCard extends LinearLayout {
+public class JunctionCard extends HorizontalScrollView {
 
-    public JunctionCard(Context context, AttributeSet attrs){
-        super(context, attrs);
-        LayoutInflater inflater = LayoutInflater.from(context);
-        inflater.inflate(R.layout.card_layout, this, false);
+    public JunctionCard(Context context) {
+        super(context);
+        float Ywidth=context.getResources().getDisplayMetrics().ydpi;
+        float Xwidth=context.getResources().getDisplayMetrics().xdpi;
+        ViewGroup.LayoutParams layoutParams=new ViewGroup.LayoutParams()
     }
 }
